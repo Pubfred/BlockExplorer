cceeffb0d590dc346238dacfe47a800449fa86ae
Pubfred <pubfree@free.fr>
2018-03-07 05:22:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-var api = 'http://XXX.XXXX.XX.XX:31006'; //replace X for the IP where daemon is hosted or localhost
+var api = 'http://10.142.0.2:21006'; //replace X for the IP where daemon is hosted or localhost
 var blockTargetInterval = 120;
 var coinUnits = 1000000;
-var symbol = 'FBF';
+var symbol = 'ZEON';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
